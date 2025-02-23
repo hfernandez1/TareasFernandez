c141cd9250be88ce77c9aa2eedd71c9e90fecb9c
hfernandez1 <hfernandez@estudiantec.cr>
2025-02-23 12:41:21 -0600

    Agregado archivo con errores de Flake8

---
+++ Tarea 1/errores_flake.py
@@ -0,0 +1,3 @@
+x =5  # Falta un espacio despues del "="
+print(x +  10)  # Hay mas de un espacio despues del "+"
+print("Esta línea es demasiado larga y supera los 79 caracteres lo que causará un error con flake8")
